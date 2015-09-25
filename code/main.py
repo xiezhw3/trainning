0d8f83024cc35604e183e331a479fd4bcaf015a9
秦鹏 <qinpeng@corp.netease.com>
2015-09-25 17:06:14 +0800

    fix

--- code/main.py
+++ code/main.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from redis import Redis
+import socket
 import os
 
 import config
 
 app = Flask(__name__)
-redis = Redis(host=config.REDIS_ADDR, port=config.REDIS_PORT)
+redis = Redis(host='redis', port=6379)
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    return 'Hello World! I have been seen %s times from %s.' % (redis.get('hits'), socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host=config.APP_HOST, port=config.APP_PORT, debug=config.DEBUG)
