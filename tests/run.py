af066998532462dc9e740662e5fb5dff9fe0bcd2
gzhongzenglin <gzhongzenglin@corp.netease.com>
2015-11-05 10:57:57 +0800

    add unittest script

---
+++ tests/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:     run.py
+Author:       neilhong
+@contact:     gzhongzenglin@corp.netease.com
+@date:        2015-11-05 10:57:20
+
+Description:
+
+Changelog:
+
+'''
