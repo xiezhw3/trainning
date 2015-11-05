1997f578d7795249851a62c3ab36f00274400f71
gzhongzenglin <gzhongzenglin@corp.netease.com>
2015-11-05 10:30:08 +0800

    update repo

--- code/main.py
+++ code/main.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from redis import Redis
 import socket
 import os
 
 import config
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times from %s.' % (redis.get('hits'), socket.gethostname())
+    return 'Hello World! application run successfully from %s' % (socket.gethostname(), )
 
 if __name__ == "__main__":
     app.run(host=config.APP_HOST, port=config.APP_PORT, debug=config.DEBUG)
